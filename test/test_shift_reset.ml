2f56163a86580e2346c1462d4827cdf929e03520
Darius Foo <darius.foo.tw@gmail.com>
2025-10-31 13:37:34 +0800

    Start on regex example

--- test/test_shift_reset.ml
+++ test/test_shift_reset.ml
@@ -68,3 +68,79 @@ let%expect_test "shift0 and control" =
   (match r with 3 -> "shift" | 1 -> "shift0" | 2 -> "control" | _ -> "??")
   |> test;
   [%expect {| shift |}]
+
+type regex =
+  | Emp
+  | Atom of int
+  | Seq of regex * regex
+  | Disj of regex * regex
+
+let interp r ns : bool =
+  let rec visit r ns : int list option =
+    match r with
+    | Emp -> Some ns
+    | Atom a -> (match ns with b :: ns1 when a = b -> Some ns1 | _ -> None)
+    | Seq (r1, r2) ->
+      (match visit r1 ns with None -> None | Some rest -> visit r2 rest)
+    | Disj (_, _) -> failwith "?"
+  in
+  match visit r ns with Some [] -> true | _ -> false
+
+let interp_cps r ns : bool =
+  let rec visit r ns k : int list option =
+    match r with
+    | Emp -> k (Some ns)
+    | Atom a -> k (match ns with b :: ns1 when a = b -> Some ns1 | _ -> None)
+    | Seq (r1, r2) ->
+      visit r1 ns (function None -> None | Some rest -> visit r2 rest k)
+    | Disj (r1, r2) ->
+      (match visit r1 ns k with None -> visit r2 ns k | r -> k r)
+  in
+  match visit r ns Fun.id with Some [] -> true | _ -> false
+
+let interp_cps_m r ns : bool =
+  let rec visit r ns k mk : int list option =
+    match r with
+    | Emp -> k (Some ns) mk
+    | Atom a ->
+      k (match ns with b :: ns1 when a = b -> Some ns1 | _ -> None) mk
+    | Seq (r1, r2) ->
+      visit r1 ns
+        (fun v1 mk1 ->
+          match v1 with None -> None | Some rest -> visit r2 rest k mk1)
+        mk
+    | Disj (r1, r2) ->
+      (match visit r1 ns k mk with None -> visit r2 ns k mk | r -> k r mk)
+  in
+  match visit r ns (fun x mk -> mk x) Fun.id with Some [] -> true | _ -> false
+
+let%expect_test _ =
+  let test r ns =
+    let pr r = r |> [%derive.show: bool] |> print_endline in
+    (* interp r ns |> pr; *)
+    (* interp_cps r ns |> pr; *)
+    interp_cps_m r ns |> pr
+  in
+  test Emp [];
+  [%expect {| true |}];
+  test Emp [1];
+  [%expect {| false |}];
+
+  test (Atom 1) [1];
+  [%expect {| true |}];
+  test (Atom 1) [2];
+  [%expect {| false |}];
+
+  test (Seq (Emp, Atom 2)) [2];
+  [%expect {| true |}];
+  test (Seq (Atom 2, Emp)) [2];
+  [%expect {| true |}];
+  test (Seq (Atom 1, Atom 2)) [1; 2];
+  [%expect {| true |}];
+  test (Seq (Atom 1, Atom 2)) [1; 3];
+  [%expect {| false |}];
+  (* assoc *)
+  test (Seq (Seq (Atom 1, Atom 3), Atom 2)) [1; 3; 2];
+  [%expect {| true |}];
+  test (Seq (Atom 1, Seq (Atom 3, Atom 2))) [1; 3; 2];
+  [%expect {| true |}]
